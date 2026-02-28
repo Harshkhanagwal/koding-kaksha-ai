395169c31ce692bfa9aa9ba207c26fd328e66b87
harsh khanagwal <harshkhanagwal@harshs-MacBook-Air.local>
2026-02-28 12:21:46 +0530

    import error fixed from home.jsx error fixed

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -5,9 +5,7 @@ import Hero from "../../Components/Herosection/Herosection";
 import Features from "../../Components/Features/Features";
 import CTA from "../../Components/CTA/Cta";
 import HowItWorks from "../../Components/HowItWorks/HowItWorks";
-import Stats from "../../Components/Stats/Stats";
 import TrustSection from "../../Components/Trust/TrustSection";
-import DarkSection from "../../Components/Darksection/DarkSection";
 import ProjectOverview from "../../Components/ProjectOverView/ProjectOverView";
 import LearningSystem from "../../Components/LearningSystem/LearningSystem";
 
